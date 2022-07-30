dea2c38903133dc268e72988c0ac1f72a6e9345f
Rioto Nagano <goldenkamui0927@gmail.com>
2022-07-30 12:18:13 +0900

    emailカラムにindexオプションの付与

--- db/migrate/20220730025317_create_users.rb
+++ db/migrate/20220730025317_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :password_digest, null: false
 
       t.timestamps
+      t.index :email, unique: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_07_30_025317) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
