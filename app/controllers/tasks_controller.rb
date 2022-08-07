33969698a334b102ef25a20452e7d7a2e6d9890d
Rioto Nagano <goldenkamui0927@gmail.com>
2022-08-07 12:42:32 +0900

    Revert "ログインしているユーザーのみTaskデータを操作できる"
    This reverts commit e57b0ab115f7d9ed7f22f847a69aa69aab8d29b9.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = current_user.tasks
+    @tasks = Task.all
   end
 
   def new
@@ -8,7 +8,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = current_user.tasks.new(task_params)
+    @task = Task.new(task_params)
     if @task.save
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
     else
@@ -17,21 +17,21 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
+    task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
+    task = Task.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
