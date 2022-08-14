5a8cce29c390834264e3c4ecf57651c3915454f7
rioto.nagano <kingdom950927@gmail.com>
2022-08-14 13:38:34 +0900

    テキストの書き忘れの修正

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -2,7 +2,7 @@ class TaskMailer < ApplicationMailer
   def creation_email(task)
   @task = task
   mail(
-    subject: 'タスク完了メール',
+    subject: 'タスク作成完了メール',
     to: 'user@example.com',
     from: 'taskleaf@example.com'
   )
