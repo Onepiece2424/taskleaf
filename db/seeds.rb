f88acab9617fbbddc84fa55eb76236d13d50003b
rioto.nagano <kingdom950927@gmail.com>
2022-08-20 19:00:32 +0900

    スペルミスの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@
 #   password_confirmation: 'password'
 # )
 
-User.find_or_create_by!(email: 'admin@example.co') do |user|
+User.find_or_create_by!(email: 'admin@example.com') do |user|
   user.name = 'admin'
   user.admin = true
   user.password = 'password'
